658ba76738ee0fa8c30abfe192b75d8a15af36b0
Roberto Zagni <rz70rz70@gmail.com>
2020-01-23 14:33:39 +0200

    Added Spring Boot, Spring Web & JPA, Lombok and H2 dependencies and bare-bone app & test.

---
+++ src/main/java/com/robertozagni/SPYTM/data/collector/SpytmDataCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.robertozagni.SPYTM.data.collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpytmDataCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpytmDataCollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robertozagni/SPYTM/data/collector/SpytmDataCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertozagni.SPYTM.data.collector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpytmDataCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
