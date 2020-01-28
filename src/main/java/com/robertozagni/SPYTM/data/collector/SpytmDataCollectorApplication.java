2ab926f34c58d11151bdd256b6a2138fddc5ace9
Roberto Zagni <rz70rz70@gmail.com>
2020-01-28 18:04:33 +0200

    STM-6 Set up local PG instance for Test and Prod. Created users and DBs; set up Spring config; updated README.md

--- src/main/java/com/robertozagni/SPYTM/data/collector/SpytmDataCollectorApplication.java
+++ src/main/java/com/robertozagni/SPYTM/data/collector/SpytmDataCollectorApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpytmDataCollectorApplication {
 
 	public static void main(String[] args) {
