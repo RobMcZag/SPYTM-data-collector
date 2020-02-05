44e293dea4d9fb4cdb06f07b60f6fcdc318a3000
Roberto Zagni <rz70rz70@gmail.com>
2020-02-05 20:44:51 +0200

    Adding Snowflake to the runner to save the time series after downloading them.

--- src/test/java/com/robertozagni/SPYTM/data/collector/service/TimeSerieStorageServiceTest.java
+++ src/test/java/com/robertozagni/SPYTM/data/collector/service/TimeSerieStorageServiceTest.java
@@ -1,6 +1,5 @@
 package com.robertozagni.SPYTM.data.collector.service;
 
-import com.robertozagni.SPYTM.data.collector.downloader.StockDataDownloaderRunner;
 import com.robertozagni.SPYTM.data.collector.downloader.alphavantage.AlphaVantageDownloaderTestHelper;
 import com.robertozagni.SPYTM.data.collector.model.TimeSerie;
 import com.robertozagni.SPYTM.data.collector.model.TimeSerieMetadata;
