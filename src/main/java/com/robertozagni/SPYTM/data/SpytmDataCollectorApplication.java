73d48cf010191ca8b07729750e8f980a475cec91
Roberto Zagni <rz70rz70@gmail.com>
2020-02-07 20:43:40 +0200

    Moving app to two-repo structure
    Moved JPA repositories definition to Collector Config and limited scanning for this JPA repos to collector sub-packages.

--- src/main/java/com/robertozagni/SPYTM/data/SpytmDataCollectorApplication.java
+++ src/main/java/com/robertozagni/SPYTM/data/SpytmDataCollectorApplication.java
@@ -2,12 +2,8 @@ package com.robertozagni.SPYTM.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableJpaRepositories
-@EnableTransactionManagement
 public class SpytmDataCollectorApplication {
 
 	public static void main(String[] args) {
