71ab37c4a69d60a05e6bea3b319a9d3c35f09311
Roberto Zagni <rz70rz70@gmail.com>
2020-02-27 12:25:54 +0200

    FIX Explicitly picking the physical_naming_strategy to use camel case (SpringPhysicalNamingStrategy) and desired schema.

--- src/main/java/com/robertozagni/SPYTM/data/collector/CollectorConfig.java
+++ src/main/java/com/robertozagni/SPYTM/data/collector/CollectorConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
@@ -54,24 +54,16 @@ public class CollectorConfig {
                 .build();
     }
 
-/* TO have snake_case names for tables and columns in the DB the following, as by default, must be used by the EMF
-        Map<String, Object> properties = factory.getJpaPropertyMap();
-        properties.put("hibernate.physical_naming_strategy", SpringPhysicalNamingStrategy.class.getName());
-        properties.put("hibernate.implicit_naming_strategy", SpringImplicitNamingStrategy.class.getName());
-*/
     @Primary
     @Bean(name = "collectorEntityManagerFactory")
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(@Qualifier("collectorDatasource")
-                                                                                   DataSource dataSource) {
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            @Qualifier("collectorDatasource") DataSource dataSource,
+            EntityManagerFactoryBuilder builder) {
 
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setGenerateDdl(true);
-
-        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-        factory.setJpaVendorAdapter(vendorAdapter);
-        factory.setPackagesToScan("com.robertozagni.SPYTM.data.collector");
-        factory.setDataSource(dataSource);
-        return factory;
+        return builder
+                .dataSource(dataSource)
+                .packages("com.robertozagni.SPYTM.data.collector")
+                .build();
     }
 
     @Primary
