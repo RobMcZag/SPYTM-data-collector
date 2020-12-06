657ef3629c88c65fd8951888abded5968e016613
Roberto Zagni <rz70rz70@gmail.com>
2020-12-06 12:04:08 +0200

    Fixed tests by picking the correct provider for the static JSON being used.

--- src/test/java/com/robertozagni/SPYTM/data/collector/service/DataCollectorServiceTest.java
+++ src/test/java/com/robertozagni/SPYTM/data/collector/service/DataCollectorServiceTest.java
@@ -2,6 +2,8 @@ package com.robertozagni.SPYTM.data.collector.service;
 
 import com.robertozagni.SPYTM.data.collector.downloader.alphavantage.AVTimeSerie;
 import com.robertozagni.SPYTM.data.collector.downloader.alphavantage.AlphaVantageDownloaderTestHelper;
+import com.robertozagni.SPYTM.data.collector.downloader.yahoo.YFv8TimeSerie;
+import com.robertozagni.SPYTM.data.collector.downloader.yahoo.YahooStockDownloaderTestHelper;
 import com.robertozagni.SPYTM.data.collector.model.DownloadRequest;
 import com.robertozagni.SPYTM.data.collector.model.QuoteProvider;
 import com.robertozagni.SPYTM.data.collector.model.QuoteType;
@@ -24,31 +26,47 @@ public class DataCollectorServiceTest {
     @Mock RestTemplate restTemplate;
     @Mock TimeSerieStorageService timeSerieStorageService;
     @Mock SnowflakeStorageService snowflakeStorageService;
-    private DataCollectorService downloaderRunner;
+    private DataCollectorService dataCollectorService;
 
     @BeforeEach
     void init() {
         MockitoAnnotations.initMocks(this);
         DataDownloaderService dataDownloaderService = new DataDownloaderService(restTemplate);
-        downloaderRunner = new DataCollectorService(dataDownloaderService, timeSerieStorageService, snowflakeStorageService);
+        dataCollectorService = new DataCollectorService(dataDownloaderService, timeSerieStorageService, snowflakeStorageService);
     }
 
     @Test
-    void download_and_storage_repository_are_invoked_the_right_number_of_times() throws Exception {
-        String[] args = {"MSFT", "AAPL", "XYZ"};
+    void download_and_storage_repository_are_invoked_the_right_number_of_times_for_AV() throws Exception {
+        String[] args = {"MSFT", "AAPL", "XYZ", QuoteProvider.APLPHA_VANTAGE.toString()};
         AVTimeSerie msftTimeSerie = AlphaVantageDownloaderTestHelper.loadSampleMSFT_AVTimeSerie();
 
         when(restTemplate.getForObject(anyString(), eq(AVTimeSerie.class))).thenReturn(msftTimeSerie);
         when(timeSerieStorageService.save(any(TimeSerie.class))).thenReturn(msftTimeSerie.toModel());
 
-        downloaderRunner.run(args);
+        dataCollectorService.run(args);
 
-        verify(restTemplate, times(args.length))
+        verify(restTemplate, times(args.length - 1))
                 .getForObject(anyString(), eq(AVTimeSerie.class));
 
-        verify(timeSerieStorageService, times(args.length))
+        verify(timeSerieStorageService, times(args.length - 1))
                 .save(any(TimeSerie.class));
+    }
 
+    @Test
+    void download_and_storage_repository_are_invoked_the_right_number_of_times_for_YF() throws Exception {
+        String[] args = {"MSFT", "AAPL", "XYZ", QuoteProvider.YAHOO_FINANCE.toString()};
+        YFv8TimeSerie timeSerie = YahooStockDownloaderTestHelper.loadSampleAAPL_YFv8TimeSerie();
+
+        when(restTemplate.getForObject(anyString(), eq(YFv8TimeSerie.class))).thenReturn(timeSerie);
+        when(timeSerieStorageService.save(any(TimeSerie.class))).thenReturn(timeSerie.toModel());
+
+        dataCollectorService.run(args);
+
+        verify(restTemplate, times(args.length - 1))
+                .getForObject(anyString(), eq(YFv8TimeSerie.class));
+
+        verify(timeSerieStorageService, times(args.length - 1))
+                .save(any(TimeSerie.class));
     }
 
     @Test
