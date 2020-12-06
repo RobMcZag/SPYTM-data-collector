63a9c398bd319c4eae5c5c7efe8cf90928072e47
Roberto Zagni <rz70rz70@gmail.com>
2020-12-06 12:10:43 +0200

    Fixed tests by picking the correct provider for the provider specific TimeSerie being used.

--- src/test-integration/java/com/robertozagni/SPYTM/data/SpytmDataCollectorApplicationTests.java
+++ src/test-integration/java/com/robertozagni/SPYTM/data/SpytmDataCollectorApplicationTests.java
@@ -2,14 +2,13 @@ package com.robertozagni.SPYTM.data;
 
 import com.robertozagni.SPYTM.data.collector.downloader.alphavantage.AVTimeSerie;
 import com.robertozagni.SPYTM.data.collector.downloader.alphavantage.AVTimeSerieMetadata;
+import com.robertozagni.SPYTM.data.collector.model.QuoteProvider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
 
 import static org.mockito.Mockito.*;
 
@@ -28,11 +27,11 @@ class SpytmDataCollectorApplicationTests {
 
 	@Test
 	void commandLineRunner_bean_exists_and_calls_correct_template() throws Exception {
-		AVTimeSerie emptyAVTimeSerie = makeEmptyTimeSerieData();
-		when(mockTemplate.getForObject(anyString(), eq(AVTimeSerie.class)))
-				.thenReturn(emptyAVTimeSerie);
 
-		commandLineRunner.run("DAILY_ADJUSTED", "MSFT", "AAPL");
+		when( mockTemplate.getForObject(anyString(), eq(AVTimeSerie.class)) )
+			  .thenReturn(makeEmptyTimeSerieData());
+
+		commandLineRunner.run("DAILY_ADJUSTED", "MSFT", "AAPL", QuoteProvider.APLPHA_VANTAGE.name());
 
 		verify(mockTemplate, times(2)).getForObject(anyString(), eq(AVTimeSerie.class));
 	}
