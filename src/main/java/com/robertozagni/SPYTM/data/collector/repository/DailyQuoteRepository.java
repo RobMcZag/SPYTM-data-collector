11d4150fe4846e64ee18eb97a70210e0a2b5b9fc
Roberto Zagni <rz70rz70@gmail.com>
2021-01-02 18:49:30 +0200

    Added query method to load latest date for each provider, quote_type, symbol combination

--- src/main/java/com/robertozagni/SPYTM/data/collector/repository/DailyQuoteRepository.java
+++ src/main/java/com/robertozagni/SPYTM/data/collector/repository/DailyQuoteRepository.java
@@ -3,10 +3,21 @@ package com.robertozagni.SPYTM.data.collector.repository;
 import com.robertozagni.SPYTM.data.collector.model.DailyQuote;
 import com.robertozagni.SPYTM.data.collector.model.QuoteProvider;
 import com.robertozagni.SPYTM.data.collector.model.QuoteType;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface DailyQuoteRepository extends CrudRepository<DailyQuote, DailyQuote.DailyQuoteId> {
     List<DailyQuote> findBySymbolAndQuotetypeAndProvider(String symbol, QuoteType type, QuoteProvider provider);
+
+    @Query(value =
+            "SELECT * FROM spytm.daily_quote\n" +
+            "WHERE (provider, quotetype, symbol, date) IN (\n" +
+            "    SELECT provider, quotetype, symbol, max(date)\n" +
+            "    FROM spytm.daily_quote\n" +
+            "    GROUP BY provider, quotetype, symbol\n" +
+            "    )"
+            , nativeQuery = true)
+    List<DailyQuote> getLatestQuoteByDate();
 }
